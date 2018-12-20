e044b751cd941a144628453ca1f3220436a3b486
Balaji D R <drbalaji97@gmail.com>
2018-12-21 01:11:48 +0530

    added dashboard template

--- frontend/dis-assist-er/src/index.js
+++ frontend/dis-assist-er/src/index.js
@@ -1,12 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { HashRouter, Route, Switch } from "react-router-dom";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import indexRoutes from "../src/routes/index.jsx";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./assets/css/animate.min.css";
+import "./assets/sass/light-bootstrap-dashboard.css?v=1.2.0";
+import "./assets/css/demo.css";
+import "./assets/css/pe-icon-7-stroke.css";
+
+ReactDOM.render(
+  <HashRouter>
+    <Switch>
+      {indexRoutes.map((prop, key) => {
+        return <Route to={prop.path} component={prop.component} key={key} />;
+      })}
+    </Switch>
+  </HashRouter>,
+  document.getElementById("root")
+);
