7a14e3d4ba39df96ae1cc3382fe7a81de911bd2a
Balaji D R <drbalaji97@gmail.com>
2019-01-31 01:50:49 +0530

    changed admin url

--- disnotify/disnotify/settings.py
+++ disnotify/disnotify/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/djangostatic/'
 
 
 CORS_ORIGIN_ALLOW_ALL = True
