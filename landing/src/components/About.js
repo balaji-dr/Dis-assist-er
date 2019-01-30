7ef85982f04cde43ef623115db71f98672ba6f50
karkeykk <karthiorton@gmail.com>
2019-01-31 01:32:55 +0530

    Updated About section

--- landing/src/components/About.js
+++ landing/src/components/About.js
@@ -11,7 +11,7 @@ class About extends Component {
                             <div className="col-lg-10">
                                 <div className="section-title text-center">
                                     <p className="text-uppercase">About Our App</p>
-                                    <h3>A unique solution aimed at providing all sorts of supports for assisting disaster victms. It also serves as a platform for connecting the <b>volunteers and victims</b></h3>
+                                    <h3>A unique solution aimed at providing all sorts of support for assisting disaster victms. During disaster times, people struggle to find help and people who are willing to help struggle to find legitimate source of need. Our app serves as a platform for connecting the <b>volunteers and victims</b>.</h3>
                                 </div>
                             </div>
                         </div>
